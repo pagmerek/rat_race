ea5ca2ee2a49f6e614bb7587711867e21d7e3f6f
Maksymilian Demitraszek <maksymilian.demitraszek@protonmail.com>
2021-04-29 22:21:40 +0200

    test fix

--- tests/App.test.ts
+++ tests/App.test.ts
@@ -4,12 +4,14 @@ import { mocked } from 'ts-jest/utils';
 import { App } from '../src/App';
 import { ControllerStub } from './stubs/ControllerStub';
 
+
 jest.mock('sequelize')
 jest.mock('express')
 
 describe('App', () => {
 
     test('creates Sequelize instance xD', () => {
+
         const mockedSequelizeConstructor = mocked(Sequelize, true);
         new App('database_urlxD', 5555);
         expect(mockedSequelizeConstructor).toHaveBeenCalledWith('database_urlxD');
@@ -22,23 +24,8 @@ describe('App', () => {
     });
 
     test('adds controller xD', () => {
-
-        // const mockedExpress = mocked(express, true)
-        // const instance = express();
-        // const mockedExpressInstance = mocked(instance, true);
-        // mockedExpress.mockReturnValue(instance);
-
-        // const app = new App('database_urlxD', 5555);
-        // const controller = new ControllerStub();
-        // app.addController(controller);
-
-        // expect(mockedExpressInstance.use).toHaveBeenCalledWith(controller);
-        // const app = new App('database_urlxD', 5555);
-        // const controller = new ControllerStub();
-        // expect(app.addController(controller)).not.toThrow();
-//         const app = new App('database_urlxD', 5555);
-//         const controller = new ControllerStub();
-//         expect(app.addController(controller)).not.toThrow();
-
+        const app = new App('database_urlxD', 5555);
+        const controller = new ControllerStub();
+        expect(app.addController(controller)).not.toThrowError();
     })
 });
